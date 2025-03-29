14762d7d21db3392043bd0e076b95904cb9b3760
Alex <40648882+GhosttGtt@users.noreply.github.com>
2025-03-29 12:00:22 -0600

    menuJSModify

--- js/menu.js
+++ js/menu.js
@@ -7,9 +7,9 @@ function cargarMenu() {
             const menuElementFooter = document.getElementById("footer-menu");
             const currentPage = window.location.pathname.split("/").pop();
             data.menu.forEach(item => {
-                const li = document.createElement("li"); 
+                const li = document.createElement("li");
                 const a = document.createElement("a");
-                a.textContent = item.name; 
+                a.textContent = item.name;
                 a.href = item.url;
                 li.appendChild(a);
                 if (item.url === currentPage) {
@@ -19,8 +19,35 @@ function cargarMenu() {
                 menuElementMobile.appendChild(li.cloneNode(true));
                 a.classList.add("grey-text", "text-lighten-3");
                 menuElementFooter.appendChild(li.cloneNode(true));
+
+                // Add submenu if it exists
+                if (item.submenu && item.submenu.length > 0) {
+                    const ulSubmenu = document.createElement("ul");
+                    ulSubmenu.classList.add("submenu");
+                    
+
+                    item.submenu.forEach(subItem => {
+                        const subLi = document.createElement("li");
+                        const subA = document.createElement("a");
+                        subA.textContent = subItem.name;
+                        subA.href = subItem.url;
+                        subLi.appendChild(subA);
+                        ulSubmenu.appendChild(subLi);
+                    });
+                    li.appendChild(ulSubmenu);
+
+                    // Show submenu on hover
+                    li.addEventListener("mouseover", () => {
+                        ulSubmenu.style.display = "block";
+                    });
+
+                    // Hide submenu when mouse leaves
+                    li.addEventListener("mouseout", () => {
+                        ulSubmenu.style.display = "none";
+                    });
+                }
             });
         })
-        .catch(error => console.error("Error al cargar el menú:", error)); 
+        .catch(error => console.error("Error al cargar el menú:", error));
 }
 cargarMenu();
